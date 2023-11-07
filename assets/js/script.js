cef0cc37b6dd66fbb9d1ce708eb2c12f4d4909cd
developerorin2002 <developerorin2002@gmail.com>
2023-11-07 16:17:40 +0600

    best product done

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,15 @@
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
     });
+    $('.best-products-sliders').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        arrows:false,
+        dots:false,
+        
+    });
 
 
 
