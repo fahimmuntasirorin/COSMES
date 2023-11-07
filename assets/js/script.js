9377d220fece6d8fb2f209656adf43f5d7518680
developerorin2002 <developerorin2002@gmail.com>
2023-11-07 16:31:41 +0600

    hm2 brands section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,32 @@
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay:true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+        autoplay: true,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
     });
+    // best product slider
     $('.best-products-sliders').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay:true,
-        arrows:false,
-        dots:false,
-        
-    });
+        autoplay: true,
+        arrows: false,
+        dots: false,
 
+    });
+    // brands slider
+
+    $('.hm2-brands').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        centerMode:true
+
+    });
 
 
 })(jQuery)
