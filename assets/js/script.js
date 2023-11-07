4965a25f5e52922af6eabc003c169e821132cdda
developerorin2002 <developerorin2002@gmail.com>
2023-11-07 14:41:55 +0600

    hm2 banner end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-(function($){
+(function ($) {
 
     //1. Data Background Set
     $('[data-background]').each(function () {
@@ -9,6 +9,15 @@
     //nice select
     $(".nice_select").niceSelect();
 
+    // product slider
+    $('.products-sliders').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
 
 
 
